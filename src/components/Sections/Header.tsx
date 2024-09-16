959a789c11865469d46dc5dc23e9574105113cfe
Yihan Huang <huangehan@gmail.com>
2024-09-16 17:20:06 +1000

    color

--- src/components/Sections/Header.tsx
+++ src/components/Sections/Header.tsx
@@ -34,7 +34,7 @@ const DesktopNav: FC<{navSections: SectionId[]; currentSection: SectionId | null
     const activeClass = classNames(baseClass, 'text-stone-400');
     const inactiveClass = classNames(baseClass, 'text-neutral-100');
     return (
-      <header className="fixed top-0 z-50 hidden w-4/5 bg-pink-200/50 p-4 backdrop-blur sm:block" id={headerID}>
+      <header className="fixed top-0 z-50 hidden w-4/5 p-4 sm:block" id={headerID}>
         <nav className="flex justify-center gap-x-8">
           {navSections.map(section => (
             <NavItem
@@ -61,13 +61,13 @@ const MobileNav: FC<{navSections: SectionId[]; currentSection: SectionId | null}
 
     const baseClass =
       'p-2 rounded-md first-letter:uppercase transition-colors duration-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-pink-500';
-    const activeClass = classNames(baseClass, 'bg-neutral-900 text-white font-bold');
-    const inactiveClass = classNames(baseClass, 'text-neutral-200 font-medium');
+    const activeClass = classNames(baseClass, 'bg-neutral-800 text-white font-bold');
+    const inactiveClass = classNames(baseClass, 'text-neutral-50 font-medium');
     return (
       <>
         <button
           aria-label="Menu Button"
-          className="fixed right-2 top-2 z-40 rounded-md bg-pink-200 p-2 ring-offset-gray-800/60 hover:bg-pink-200 focus:outline-none focus:ring-0 focus-visible:ring-2 focus-visible:ring-pink-200 focus-visible:ring-offset-2 sm:hidden"
+          className="fixed right-2 top-2 z-40 rounded-md bg-[#333ca8] p-2 ring-offset-gray-800/60 hover:bg-pink-200 focus:outline-none focus:ring-0 focus-visible:ring-2 focus-visible:ring-pink-200 focus-visible:ring-offset-2 sm:hidden"
           onClick={toggleOpen}>
           <Bars3BottomRightIcon className="h-8 w-8 text-white" />
           <span className="sr-only">Open sidebar</span>
@@ -92,7 +92,7 @@ const MobileNav: FC<{navSections: SectionId[]; currentSection: SectionId | null}
               leave="transition ease-in-out duration-300 transform"
               leaveFrom="translate-x-0"
               leaveTo="-translate-x-full">
-              <div className="relative w-4/5 bg-pink-200">
+              <div className="relative w-full bg-pink-300">
                 <nav className="mt-5 flex flex-col gap-y-2 px-2">
                   {navSections.map(section => (
                     <NavItem
